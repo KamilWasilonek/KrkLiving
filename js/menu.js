2d06364257112be4a9a07fed975be4a4536b64f6
Kamil Wasilonek <kamilwasilonek1@gmail.com>
2019-01-30 21:45:36 +0100

    Menu responsive and font size

--- js/menu.js
+++ js/menu.js
@@ -14,20 +14,33 @@ function toggleMenu() {
     } else {
         menuBtn.classList.remove('close');
         menu.classList.remove('show');
-        
+
         showMenu = false;
     }
 }
 
-
 $(document).ready(function () {
+
+    if(window.innerWidth > 768){
+        $(".apartments #description-1").html("KrakowLiving dysponuje ponadto 9 wysokiej klasy apartamentami zlokalizowanych w Starym Mieście i na Kazimierzu.<br> Apartamenty w naszej ofercie są odpowiednie zarówno dla osób podróżujących służbowo jak i dla rodzin spędzających wakacje. Wszystkie posiadają dostęp do prywatnej łazienki, kuchni i sprzętu RTV.");
+    }
+
     $(window).scroll(function () {
+        console.log(window.innerWidth);
         if ($(window).scrollTop() >= 50) {
             $('#menu-bar').css("background", "rgb(0,0,0,0.7)");
-            $('#menu-nav').css("background", "rgb(0,0,0,0.7)");
+            if (window.innerWidth < 992) {
+                $('#menu-nav').css("background", "rgb(0,0,0,0.7)");
+            }
+            $('#logo').css("opacity", "0");
+            $('#contact-text').css("opacity", "1");
         } else {
             $('#menu-bar').css("background", "rgb(0,0,0,1)");
-            $('#menu-nav').css("background", "rgb(0,0,0,1)");
+            if (window.innerWidth < 992) {
+                $('#menu-nav').css("background", "rgb(0,0,0,1)");
+            }
+            $('#logo').css("opacity", "1");
+            $('#contact-text').css("opacity", "0");
         }
     })
 })
